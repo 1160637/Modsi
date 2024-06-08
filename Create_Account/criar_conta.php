85bab88189253a42baf7e85009003a5f446f1d76
1160637 <1160637@isep.ipp.pt>
2024-06-08 21:11:19 +0100

    add criar conta php

--- Create_Account/criar_conta.php
+++ Create_Account/criar_conta.php
@@ -1,53 +1,31 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name_id'];
-    $nascimento = $_POST['Nascimento']
+    $nascimento = $_POST['Nascimento'];
     $email = $_POST['email'];
-    $telemovel = $_POST['telemovel']
+    $telemovel = $_POST['telemovel'];
     $password = $_POST['passwd'];
     $confirm_password = $_POST['conf_passwd'];
 
-    // Verifica se email e password não estão vazios
-    if (empty($name_sign_up) || empty($email_sign_up) || empty($password_sign_up)) {
-        echo "Nome, email e password são obrigatórios!";
-        exit;
+    // Validate input
+    if ($password !== $confirm_password) {
+        exit();
     }
 
-    //Verifica de passwd são iguais
-    if ($password != $confirm_password){
-        echo "PASSWORD DIFERENTES"
-        exit;
-    }
     // Database connection
-    $servername = 'ave.dee.isep.ipp.pt';
-    $username = '1160637';
-    $dbpassword = 'admin';
-    $dbname = '1160637';
-
     $conn= new mysqli("ave.dee.isep.ipp.pt", "1160637", "admin", "1160637");
     if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
     }
 
-    $query = $conn->prepare("INSERT INTO 'Clientes' (`Nome`, `Idade`, `Telemovel`, `Email`, `password`) VALUES (?, ?, ?, ?, ?)");
-    if ($query === false) {
-        die("Prepare failed: " . $conn->error);
-    }
-
-    $query->bind_param("sssss", $name, $nascimento, $email, $telemovel, $password); //sss é o número de parâmetros
-    $execval = $query->execute();
-
-    if ($execval) {
-        echo "<script>window.location.href = 'Interface Inicial.html'</script>";
-    } else {
-        echo "Falha no registo: " . $query->error;
+    $query = $conn->prepare("INSERT INTO Clientes (Nome, Idade, Telemovel, Email, password) VALUES (?, ?, ?, ?, ?)");
+    $query->bind_param("sssss", $name, $nascimento, $telemovel, $email, $password);
+    if ($query->execute()) {
+        echo "New record created successfully";
+    } 
+    else {
+        echo "Error: " . $query->error;
     }
 
     $query->close();
     $conn->close();
-}
 ?>
---
+++ Create_Account/print_values.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<html>
+<body>
+<?php echo $_POST["name_id"] ?>
+<br>
+<?php echo $_POST["email"] ?>
+<br>
+<?php echo $_POST["passwd"] ?>
+<br>
+</body>
+</html>
