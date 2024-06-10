170f9cc0ba59d69b47a2a0bb2a482dc7411b3729
1160637 <1160637@isep.ipp.pt>
2024-06-10 13:03:53 +0100

    alterar vagas hoteis func

--- database_tools/funcs.php
+++ database_tools/funcs.php
@@ -55,13 +55,13 @@
         elseif($partida == 2){
             $partida = 'Espanha';
         }
-        elseif($partida == 1){
+        elseif($partida == 3){
             $partida = 'Itália';
         }
-        elseif($partida == 1){
+        elseif($partida == 4){
             $partida = 'França';
         }
-        elseif($partida == 1){
+        elseif($partida == 5){
             $partida = 'Malta';
         }
 
@@ -69,5 +69,57 @@
         $conn->close();
         return $partida;
     }
-
 ?>
+
+<?php
+    function alterarVagasHoteis($Destino, $pensão, $num_reservas)
+    {
+        session_start();
+        $conn= new mysqli("ave.dee.isep.ipp.pt", "1160637", "admin", "1160637");
+        if ($conn->connect_error) {
+          die("Connection failed: " . $conn->connect_error);
+        }
+
+        if($Destino == 1){
+            $Destino = 'Portugal';
+        }
+        elseif($Destino == 2){
+            $Destino = 'Espanha';
+        }
+        elseif($Destino == 3){
+            $Destino = 'Itália';
+        }
+        elseif($Destino == 4){
+            $Destino = 'França';
+        }
+        elseif($Destino == 5){
+            $Destino = 'Malta';
+        }
+
+        $query = $conn->prepare("SELECT Vagas FROM Hoteis WHERE Pensão = ? and hoteis_id = ?");
+        if ($query === false) {
+            die("Prepare failed: " . $conn->error);
+        }
+        $query->bind_param("si", $pensão, $Destino);
+        $query->execute();                
+        $result = $query->get_result();
+        $row = $result->fetch_assoc();
+        $vagas = $row['Vagas'];
+        if($vagas < $num_reservas){
+            echo "<script>alert('Não há vagas suficientes!'); window.location.href = '../main.php'</script>";
+        }
+        else{
+            $query = $conn->prepare("UPDATE Hoteis SET Vagas= ? WHERE Pensão = ? AND  hoteis_id = ? ");
+            if ($query === false) {
+                die("Prepare failed: " . $conn->error);
+            }
+                $new_vagas = $vagas - $num_reservas;
+                $query->bind_param("isi",$new_vagas, $pensão, $Destino);
+                $query->execute(); 
+        }
+
+        $query->close();
+        $conn->close();
+    }
+?>
+
